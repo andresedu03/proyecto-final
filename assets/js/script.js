4b0db0d743877c917ae4443d19c8dc0c37fea78e
Andres <andreseduardo1206@gmail.com>
2020-03-04 20:13:32 -0300

    se inserto el smooth scrolling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,27 @@
-$(function () {
+$(function(){
+$("a").click(function(event) {
+
+
+if (this.hash !== "") {
+
+event.preventDefault();
+
+var gato = this.hash;
+
+
+
+$('html, body').animate({
+scrollTop: $(gato).offset().top
+}, 800, function(){
+
+
+window.location.hash = gato;
+});
+}
+});
+
+
   $('[data-toggle="tooltip"]').tooltip()
-})
+
+
+});
